036864615c8b5bb8836c3d1743be2d091f784e90
sergeyesp <sergey_esp@i.ua>
2023-07-22 22:23:08 +0300

    Lesson-3

---
+++ app/src/main/java/com/example/newsapp/data/api/NewsService.kt
@@ -0,0 +1,21 @@
+package com.example.newsapp.data.api
+
+import com.example.newsapp.utils.Constants.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsService {
+
+    @GET("/v2/everything")
+    suspend fun getEverything(
+        @Query("q") query: String,
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+
+    suspend fun getHeadLines(
+        @Query("country") countryCode: String = "ua",
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+}
---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "1cfe21dc3421454da1c1981f94ea8359"
+    }
+}
